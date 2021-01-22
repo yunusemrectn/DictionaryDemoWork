2a3eb541ee0ada28b090ca565fc5efa9e7c790e5
yunusemrectn <77715994+yunusemrectn@users.noreply.github.com>
2021-01-22 10:48:58 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            MyList<string> names = new MyList<string>();
+            names.Add("Yunus Emre");
+            names.Add("Ali");
+            foreach (var item in names.Items)
+            {
+                Console.WriteLine(item);
+            }
+            */
+            cMyDictionary<int, string> users = new cMyDictionary<int, string>();
+            users.Add(1, "Yunus Emre");
+            users.Add(2, "Ali");
+            users.Add(3, "Batuhan");
+            users.Add(4, "Hüseyin");
+            users.Add(5, "Çetin");
+            users.Add(6, "Eren");
+            for (int i = 0; i < users.NKey.Length; i++)
+            {
+                Console.WriteLine("No : {0} -> Adı : {1}", users.NKey[i], users.NValue[i]);
+            }
+            Console.WriteLine("\nKayıtlı kullanıcı sayısı : {0}",users.Length);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ cMyDictionary.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    class cMyDictionary<K,V>
+    {
+        K[] nKey;
+        V[] nValue;
+        public cMyDictionary()
+        {
+            nKey = new K[0];
+            nValue = new V[0];
+        }
+        public void Add(K gKey, V gValue)
+        {
+            K[] kArray = nKey;
+            V[] vArray = nValue;
+            nKey = new K[nKey.Length+1];
+            nValue = new V[nValue.Length + 1];
+            for (int i = 0; i < kArray.Length; i++)
+            {
+                nKey[i] = kArray[i];
+                nValue[i] = vArray[i];
+            }
+            nKey[nKey.Length - 1] = gKey;
+            nValue[nValue.Length - 1] = gValue;
+        }
+        public int Length
+        {
+            get { return nKey.Length; }
+        }
+        public K[] NKey
+        {
+            get { return nKey; }
+        }
+        public V[] NValue
+        {
+            get { return nValue; }
+        }
+    }
+}
